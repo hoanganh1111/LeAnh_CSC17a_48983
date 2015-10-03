894cf8fb2493920cd0d0f960f57bb3c70eb2f66c
Anh Le <anhhoangle1111@gmail.com>
2015-10-02 20:13:56 -0700

    Changing the code, add some comments

--- Book/CH9Prob7/main.cpp
+++ Book/CH9Prob7/main.cpp
@@ -11,42 +11,44 @@ using namespace std;
 
 
 // Function prototypes
- void arrSelectSort(int *[], int);
+ void arrSelectSort(int [], int);
  void showArray(const int [], int);
- void showArrPtr(int *[], int);
+ void showArrPtr(int [], int);
 
  int main()
  {
 	 int NUM_DONATIONS; // Number of donations
 
+	 //Prompt for user's input of the size of the array
 	 cout << "Enter the number of donations: \n";
 	 cin >> NUM_DONATIONS;
 
+ //Setting the pointers of arrays to 0
  int* donations = NULL;  // An array containing the donation amounts.
- int* *arrPtr;	// An array of pointers to int.
+ int* arrPtr = NULL;	// An array of pointers to int.
 
+ //Allocate memories for the arrays
  donations = new int[NUM_DONATIONS];
- //*arrPtr = new int[NUM_DONATIONS];
+ arrPtr = new int[NUM_DONATIONS];
 
  //Initialize elements to 0;
  for(int count = 0; count < NUM_DONATIONS; count++)
   {
 	 donations[count] = 0;
-	  //*arrPtr[count] = {nullptr};
   }
 
+ //OUTPUT for donation amount
  cout << "Enter the donation amounts: \n";
  for(int count = 0; count < NUM_DONATIONS; count++)
  {
-	 cin >> donations[count];
+	 cin >> donations[count];	//INPUT donations in different orders
  }
 
-// Each element of arrPtr is a pointer to int. Make each
+ // Each element of arrPtr is a pointer to int. Make each
  // element point to an element in the donations array.
  for (int count = 0; count < NUM_DONATIONS; count++)
  {
- arrPtr[count] = &donations[count];
- *arrPtr[count] = donations[count];
+ arrPtr[count] = donations[count];
  }
 
  // Sort the elements of the array of pointers.
@@ -63,56 +65,59 @@ using namespace std;
 
  //Free the dynamic allocation
  delete [] donations;
- delete [] *arrPtr;
-
+ delete [] arrPtr;
+ //Set the arrays back to 0
  donations = NULL;
- *arrPtr = NULL;
+ arrPtr = NULL;
 
  return 0;
  }
 
 
 //Initialize selectionSort
-void arrSelectSort(int *arr[], int size)
- {
- int startScan, minIndex;
- int *minElem;
-
-for (startScan = 0; startScan < (size - 1); startScan++)
- {
- minIndex = startScan;
- minElem = arr[startScan];
- for(int index = startScan + 1; index < size; index++)
- {
- if (*(arr[index]) > *minElem)
+void arrSelectSort(int arr[], int size)
 {
- minElem = arr[index];
- minIndex = index;
- }
-}
- arr[minIndex] = arr[startScan];
- arr[startScan] = minElem;
- }
+	int startScan, minIndex;
+	int minElem;
+	for (startScan = 0; startScan < (size - 1); startScan++)
+	{
+		minIndex = startScan;
+		minElem = arr[startScan];
+		for(int index = startScan + 1; index < size; index++)
+		{
+			if ((arr[index]) > minElem)
+			{
+				minElem = arr[index];
+				minIndex = index;
+			}
+		}
+		arr[minIndex] = arr[startScan];
+		arr[startScan] = minElem;
+	}
 }
 
- //***********************************************************
+//***********************************************************
 // Definition of function showArray. *
 // This function displays the contents of arr. size is the *
 // number of elements. *
- //***********************************************************
-
+//***********************************************************
 void showArray(const int arr[], int size)
  {
-for (int count = 0; count < size; count++)
-cout << arr[count] << " ";
-cout << endl;
+	for (int count = 0; count < size; count++)
+		cout << arr[count] << " ";
+	cout << endl;
  }
 
-void showArrPtr(int *arr[], int size)
+//***********************************************************
+// Definition of function showArrayPtr. *
+// This function displays the contents of arr. size is the *
+// number of elements. *
+//***********************************************************
+void showArrPtr(int arr[], int size)
  {
- for (int count = 0; count < size; count++)
- cout << *(arr[count]) << " ";
-cout << endl;
+	for (int count = 0; count < size; count++)
+		cout << (arr[count]) << " ";
+	cout << endl;
  }
 
 
--- Book/Ch9prob6/main.cpp
+++ Book/Ch9prob6/main.cpp
@@ -4,72 +4,96 @@
  * Created on October 2, 2015, 12:12 AM
  */
 
-// This program shows the donations made to the United Cause
-// by the employees of CK Graphics, Inc. It displays
-// the donations in order from lowest to highest
-// and in the original order they were received.
-#include <iostream>
+//Libraries
 #include <iomanip>
+#include <iostream>
 using namespace std;
 
+
 // Function prototypes
-void SelectSort(int *, int);
-void showArray(int *, int);
+ void arrSelectSort(int [], int);
+ void showArray(const int [], int);
+ void showArrPtr(int [], int);
 
-int main()
+ int main()
+ {
+	 int NUM_DONATIONS; // Number of donations
+
+	 //Prompt for user's input of the size of the array
+	 cout << "Enter the number of donations: \n";
+	 cin >> NUM_DONATIONS;
+
+ //Setting the pointers of arrays to 0
+ int* donations = NULL;  // An array containing the donation amounts.
+ int* arrPtr = NULL;	// An array of pointers to int.
+
+ //Allocate memories for the arrays
+ donations = new int[NUM_DONATIONS];
+ arrPtr = new int[NUM_DONATIONS];
+
+ //Initialize elements to 0;
+ for(int count = 0; count < NUM_DONATIONS; count++)
+  {
+	 donations[count] = 0;
+  }
+
+ //OUTPUT for donation amount
+ cout << "Enter the donation amounts: \n";
+ for(int count = 0; count < NUM_DONATIONS; count++)
+ {
+	 cin >> donations[count];	//INPUT donations in different orders
+ }
+
+ // Each element of arrPtr is a pointer to int. Make each
+ // element point to an element in the donations array.
+ for (int count = 0; count < NUM_DONATIONS; count++)
+ {
+ arrPtr[count] = donations[count];
+ }
+
+ // Sort the elements of the array of pointers.
+ arrSelectSort(arrPtr, NUM_DONATIONS);
+
+ // Display the donations using the array of pointers. This
+ // will display them in sorted order.
+ cout << "The donations, sorted in ascending order are: \n";
+ showArrPtr(arrPtr, NUM_DONATIONS);
+
+ // Display the donations in their original order.
+ cout << "The donations, in their original order are: \n";
+ showArray(donations, NUM_DONATIONS);
+
+ //Free the dynamic allocation
+ delete [] donations;
+ delete [] arrPtr;
+ //Set the arrays back to 0
+ donations = NULL;
+ arrPtr = NULL;
+
+ return 0;
+ }
+
+
+//Initialize selectionSort
+void arrSelectSort(int arr[], int size)
 {
-	int NUM_DONATIONS; // Number of donations
-	int *donations;
-
-	cout << "Enter the number of donations: \n";
-	cin >> NUM_DONATIONS;
-
-// An array containing the donation amounts.
-donations = new int[NUM_DONATIONS];
-
-cout << "Enter the donations amount: \n";
-for(int count= 0; count < NUM_DONATIONS; count++)
-{
-	cin >> donations[count];
-}
-
-// Display the donations in their original order.
-cout << "The donations, in their original order are: \n";
-showArray(donations, NUM_DONATIONS);
-
-// Sort the elements of the array of pointers.
-SelectSort(donations, NUM_DONATIONS);
-
-// Display the donations using the array of pointers. This
-// will display them in sorted order.
-cout << "The donations, sorted in ascending order are: \n";
-showArray(donations, NUM_DONATIONS);
-
-
-delete  [] donations;
-donations = 0;
-
-return 0;
-}
-
-void SelectSort(int array[], int size)
-{
-	int startScan, minIndex, minValue;
-		for(startScan = 0; startScan <(size-1); startScan++)
+	int startScan, minIndex;
+	int minElem;
+	for (startScan = 0; startScan < (size - 1); startScan++)
+	{
+		minIndex = startScan;
+		minElem = arr[startScan];
+		for(int index = startScan + 1; index < size; index++)
 		{
-					minIndex = startScan;
-					minValue = array[startScan];
-	for(int index = startScan + 1; index < size; index++)
-		{
-			if (array[index] < minValue)
+			if ((arr[index]) < minElem)
 			{
-					minValue = array[index];
-					minIndex = index;
+				minElem = arr[index];
+				minIndex = index;
 			}
 		}
-			array[minIndex] = array[startScan];
-			array[startScan] = minValue;
-		}
+		arr[minIndex] = arr[startScan];
+		arr[startScan] = minElem;
+	}
 }
 
 //***********************************************************
@@ -77,10 +101,21 @@ void SelectSort(int array[], int size)
 // This function displays the contents of arr. size is the *
 // number of elements. *
 //***********************************************************
-
-void showArray(int array[], int size)
+void showArray(const int arr[], int size)
  {
 	for (int count = 0; count < size; count++)
-		cout << array[count] << " ";
+		cout << arr[count] << " ";
+	cout << endl;
+ }
+
+//***********************************************************
+// Definition of function showArrayPtr. *
+// This function displays the contents of arr. size is the *
+// number of elements. *
+//***********************************************************
+void showArrPtr(int arr[], int size)
+ {
+	for (int count = 0; count < size; count++)
+		cout << (arr[count]) << " ";
 	cout << endl;
  }
