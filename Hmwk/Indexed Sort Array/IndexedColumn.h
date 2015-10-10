a5a6c8574309bb8ded9dba41dabc6428a6ec95d7
Anh Le <anhhoangle1111@gmail.com>
2015-10-09 18:07:45 -0700

    Finished changing -> to dot operation

---
+++ Hmwk/Indexed Sort Array/IndexedColumn.h
@@ -0,0 +1,19 @@
+/*
+ * IndexedColumn.h
+ *
+ *  Created on: Oct 7, 2015
+ *      Author: AnhLe
+ */
+
+
+#ifndef INDEXEDCOLUMN_H
+#define	INDEXEDCOLUMN_H
+
+struct IndxCol{
+    int size;
+    int *array;
+    int *indx;
+};
+
+#endif	/* INDEXEDCOLUMN_H */
+
